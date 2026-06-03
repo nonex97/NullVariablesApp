1f8ddfaa3b9e6b7f593337ae82157adc74e1e10e
nonex97 <ngrguric97@gmail.com>
2026-06-03 16:22:20 +0200

    Understanding what null is, and how to proprerly assign it to a variable (int?, ...).

--- NullVariables/Program.cs
+++ NullVariables/Program.cs
@@ -1,4 +1,17 @@
 ﻿
 
+// null = lack of value
 
+// Haven't asked for the age yet
+//int age = 0; // should not be declared as 0, but rather as null, to indicate that the value is not yet assigned. but this one can't be null
+int? age = null; // this is a nullable int
 
+//All variables can be nullable
+bool? birthday = null;
+double? battingAverage = null;
+decimal? accountBalance = null;
+//string name = null; // string without a ? can be nullable, so we should use string? to indicate that it can be null to be more specific
+string? name = null;
+
+// Asked for the age now
+age = 0;
